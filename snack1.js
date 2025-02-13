1340b25662f8eb086ef9dbedc9d44076b23f8960
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-13 11:27:28 +0100

    add snack exercise

---
+++ snack1.js
@@ -0,0 +1,11 @@
+//? Code Question 1
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+const hamburger = { name: 'Cheese Burger', weight: 250 }
+const secondBurger = hamburger
+secondBurger.name = 'Double Cheese Burger'
+secondBurger.weight = 500
+
+console.log(hamburger.name) // ?
+console.log(secondBurger.name) // ?
