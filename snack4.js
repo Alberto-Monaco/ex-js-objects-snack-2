79fe01b9c951ab7bd687dd6b6166480ae022207b
Alberto Monaco <alberto.monaco84@gmail.com>
2025-02-13 12:22:58 +0100

    complete snack4

--- snack4.js
+++ snack4.js
@@ -19,3 +19,12 @@ const restaurant = {
 	openingDate: new Date(2025, 3, 11),
 	isOpen: false
 }
+
+const clonedChef = { ...chef }
+const clonedRestaurant = structuredClone(restaurant)
+
+console.log(clonedChef)
+console.log(clonedRestaurant)
+
+//? Risposta: per l'oggetto chef, il metodo migliore per clonare l'oggetto è utilizzare spread operator, poiché l'oggetto chef contiene solo proprietà di tipo primitivo e una funzione.
+//? Per l'oggetto restaurant, il metodo migliore per clonare l'oggetto è utilizzare structuredclone, poiché l'oggetto restaurant contiene proprietà annidate e una data.
